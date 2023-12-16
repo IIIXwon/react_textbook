c86c19751f98ba67109a07b1058d6031d40e2f6e
IIIXwon <IIIXwon@gmail.com>
2023-12-16 13:09:22 +0900

    babel로 jsx compile하기

--- hell-world/js/script.js
+++ hell-world/js/script.js
@@ -1,28 +1,27 @@
-let h1 = React.createElement('h1', null, 'hello world!');
 class HelloWorlds extends React.Component {
     render() {
-        return React.createElement(
-            'h1',
-            this.props,
-            'hello ' + this.props.frameworkName + ' world!!!'
-        );
+        return (
+            <h1>
+                {...this.props} Hello {this.props.frameworkName} word!!!
+            </h1>
+        )
     }
 }
 
 ReactDOM.render(
-    React.createElement('div',null,
-        React.createElement(HelloWorlds, {
-            id: 'emeber',
-            frameworkName: 'Ember.js',
-            title: 'A framework for creating ambitious web applications.'}),
-        React.createElement(HelloWorlds, {
-            id: 'backbone',
-            frameworkName: 'Backbone.js',
-            title: 'Backbone.js give structure to web applications.'}),
-        React.createElement(HelloWorlds, {
-            id: 'angular',
-            frameworkName: 'Angular.js',
-            title: 'Superheroic JavaScript MVW Framework'})
-    ),
+    <div>
+        <HelloWorlds
+            id='emeber'
+            frameworkName='Ember.js'
+            title='A framework for creating ambitious web applications.'/>,
+        <HelloWorlds
+            id='backbone'
+            frameworkName='Ember.js'
+            title='A framework for creating ambitious web applications.'/>,
+        <HelloWorlds
+            id='angular'
+            frameworkName='Angular.js'
+            title='Superheroic JavaScript MVW Framework'/>
+    </div>,
     document.querySelector('#content')
 )
